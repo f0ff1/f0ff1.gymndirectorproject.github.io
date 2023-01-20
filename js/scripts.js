4cd0b4718cff3395e85a5be511edb8080c491fca
f0ff1 <drewcool@mail.ru>
2023-01-20 09:53:25 +0300

    Added Gymn Director Project Site

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+$(document).ready(function(){
+    $("#body").on("click","#anchor", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
